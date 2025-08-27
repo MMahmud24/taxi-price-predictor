6afeea3034bf2e460228ce31eb4ca2d1ec8301ba
MMahmud24 <mahinmahmud2406@gmail.com>
2025-08-26 22:58:52 -0400

    price model finished

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -10,7 +10,7 @@ def train_model(path="../database/enhanced_data.csv", output="../models/fare_mod
 
     df = pd.read_csv(path)
 
-    features = ['trip_amount', 'hour', 'weekday', 'was_raining', 'event_nearby']
+    features = ['trip_distance', 'hour', 'weekday', 'was_raining', 'event_nearby']
     target = 'fare_amount'
 
     df = df.dropna(subset = features + [target])
@@ -18,4 +18,26 @@ def train_model(path="../database/enhanced_data.csv", output="../models/fare_mod
     X = df[features]
     Y = df[target]
 
-    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    model = GradientBoostingRegressor()
+    model.fit(X_train, Y_train)
+
+    y_pred = model.predict(X_test)
+
+    mae = mean_absolute_error(Y_test, y_pred)
+    mse = mean_squared_error(Y_test, y_pred)
+    r2 = r2_score(Y_test, y_pred)
+
+    print(f"Mean Absolute Error: {mae}")
+    print(f"Mean Squared Error: {mse}")
+    print(f"R2 Score: {r2}")
+
+    # os.makedirs(os.path.dirname(output), exist_ok=True)
+    # joblib.dump(model, output)
+
+    print(Y_test)
+    print(y_pred)
+
+if __name__ == "__main__":
+    train_model()
